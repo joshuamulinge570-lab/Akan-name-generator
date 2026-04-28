9ca0226dd7f3704725d795e0c38a8e99bf26d4ca
joshua <joshuamulinge570@gmail.com>
2026-04-28 12:52:19 +0300

    akan-name-generator

---
+++ script.js
@@ -0,0 +1,24 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akousa", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+
+function getAkanName() {
+    const dateofBirth = document.getElementById("dob").Value;
+    const gender = document.getElementById("gender").Value;
+
+    if (!dateofBirth || !gender) {
+        alert("Please enter your date of birth and select your gender.");
+        return;
+    }
+}
+const date = new Date(dateofBirth);
+const dayIndex = date.getDay(); //0 (Sunday) to 6 (Saturday)
+
+let akanName;
+if (gender === "male") {
+    akanName = maleNames[dayIndex];
+}
+else if (gender === "female") {
+    akanName = femaleNames[dayIndex];
+    
+}
+document.getElementById("result").textContent= 'Your Akan name is $(akanName)' ;
